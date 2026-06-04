0fe9f2d696b4b560bc820326e0b7926bc5da6e22
krupalinimje-star <krupalinimje@email.com>
2026-06-04 18:31:52 +0530

    feat: initialize Django project settings with PostgreSQL configuration and rest framework integration

--- mycreation/settings.py
+++ mycreation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nx_7bld!(on5t4y+96o&$6bq$wlj_8z!ntt^)s(+u^vdr*#ow!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
